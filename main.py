21e6cce6013eefbb65b1de4d070aa750607c9956
lopataboga <kostya270706@gmail.com>
2023-02-14 21:19:02 +0300

    new

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+import requests
+import json
 
+atoken = "efegr"
 
-if __name__ == '__main__':
+class GardenCommunication():
+    def __init__(self):
+        pass
 
-    app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
+    def tempSensor(self, number):
+        response = requests.get(f"https://dt.miet.ru/ppo_it/api/temp_hum/{number}", headers={"X-Auth-Token": atoken})
+        return response.json()['id'], response.json()['humidity'],  response.json()['temperature']
 
-    sys.exit(app.exec_())
+    def humSensor(self, number):
+        response = requests.get(f"https://dt.miet.ru/ppo_it/api/hum/{number}", headers={"X-Auth-Token": atoken})
+        return response.json()['id'], response.json()['humidity']
+
+    def windowOpen(self, state_value):
+        # response = requests.patch("https://dt.miet.ru/ppo_it/api/fork_drive/", data ={"state": state_value})
+        response = requests.request('patch', "https://dt.miet.ru/ppo_it/api/fork_drive/", data ={"state": state_value}, headers={"X-Auth-Token": atoken})
+        return response.status_code
+
+    def waterGarden(self, device_id, state_value):
+        response = requests.request('patch', "https://dt.miet.ru/ppo_it/api/watering/", data ={"id": device_id, "state": state_value}, headers={"X-Auth-Token": atoken})
+        return response.status_code
+
+    def globalWaterGarden(self, state_value):
+        response = requests.patch("https://dt.miet.ru/ppo_it/api/total_hum/", data={"state": state_value})
+        return response.status_code
+
+class MainApp():
+    def run(self):
+        print(GardenCommunication().windowOpen(1))
+        pass
+
+if __name__ == "__main__":
+    MainApp().run()
