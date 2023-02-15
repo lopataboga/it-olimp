bf799fd4b837908d8f455b66a26807533ad8b2c7
lopataboga <kostya270706@gmail.com>
2023-02-15 15:24:02 +0300

    new1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-atoken = "efegr"
 
 class GardenCommunication():
     def __init__(self):
@@ -16,19 +15,21 @@ class GardenCommunication():
         return response.json()['id'], response.json()['humidity']
 
     def windowOpen(self, state_value):
-        # response = requests.patch("https://dt.miet.ru/ppo_it/api/fork_drive/", data ={"state": state_value})
-        response = requests.request('patch', "https://dt.miet.ru/ppo_it/api/fork_drive/", data ={"state": state_value}, headers={"X-Auth-Token": atoken})
+        response = requests.patch("https://dt.miet.ru/ppo_it/api/fork_drive", params ={"state": state_value})
         return response.status_code
 
     def waterGarden(self, device_id, state_value):
-        response = requests.request('patch', "https://dt.miet.ru/ppo_it/api/watering/", data ={"id": device_id, "state": state_value}, headers={"X-Auth-Token": atoken})
+        response = requests.request('patch', "https://dt.miet.ru/ppo_it/api/watering", params ={"id": device_id, "state": state_value}, headers={"X-Auth-Token": atoken})
         return response.status_code
 
     def globalWaterGarden(self, state_value):
-        response = requests.patch("https://dt.miet.ru/ppo_it/api/total_hum/", data={"state": state_value})
+        response = requests.patch("https://dt.miet.ru/ppo_it/api/total_hum", params={"state": state_value})
         return response.status_code
 
 class MainApp():
+    def __init__(self):
+        atoken = "efegr"
+
     def run(self):
         print(GardenCommunication().windowOpen(1))
         pass
